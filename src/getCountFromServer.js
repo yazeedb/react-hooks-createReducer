371509708399ea6dc82e403aaaed2246f3b72a92
yazeedb <ybzadough@gmail.com>
2018-12-02 15:54:52 -0500

    useState version

--- src/getCountFromServer.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  const response = Math.round(Math.random() * 1000);
-
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(response);
-    }, 1000);
-  });
-};
--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { useState } from 'react';
-import useFetchData from './useFetchData';
-import getCountFromServer from './getCountFromServer';
 import './styles.css';
 
 function App() {
   const [count, setCount] = useState(0);
-  const [state, dispatch] = useFetchData();
-
-  getCountFromServer().then(console.log);
-
-  console.log(state);
 
   return (
     <div className="App">
       <h1>count: {count}</h1>
-
-      <button onClick={() => dispatch({ type: 'FETCH' })}>fetch</button>
 
       <button onClick={() => setCount(prevCount => prevCount + 1)}>Up!</button>
 
